3a0523df531ca9c1aadfca225735633110beb6e7
Said Rasinlic <saidrasinlic.jr@gmail.com>
2023-08-21 01:42:47 +0200

    Add file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,4 +6,5 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:precompile RAILS_ENV=production
 bundle exec rails assets:clean
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:create db:migrate db:seed
+bundle exec rails db:migrate
+bundle exec rails db:seed
