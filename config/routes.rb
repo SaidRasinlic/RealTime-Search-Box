90a57e686237e4ef3194e9b02d6529386411f3bf
Said Rasinlic <saidrasinlic.jr@gmail.com>
2023-08-20 23:52:25 +0200

    Add file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   # resources :search_logs
-  devise_for :users
+  # devise_for :users
   resources :search_logs, only: [:index]
   
   resources :articles do
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   end
   get 'destroy_user_cookie', to: 'application#destroy_user_cookie'
   get '/update_search_counts', to: 'articles#update_search_counts'
-  post '/track_search', to: 'tracking#track_search'
+  # post '/track_search', to: 'tracking#track_search'
   root 'articles#index'
   mount ActionCable.server => "/cable"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
